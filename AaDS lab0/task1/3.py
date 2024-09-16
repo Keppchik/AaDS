0e4f415a821e53d8df20fc86acbf52ed349bebf4
Keppchik <466353@niuitmo.ru>
2024-09-16 18:20:44 +0300

    Added directories for tasks

--- AaDS lab0/task1/3.py
+++ AaDS lab0/task1/3.py
@@ -1,8 +1,8 @@
-f = open('input.txt')
+f = open('../task1/input.txt')
 a, b = map(int,f.readline().split())
 f.close()
 if -10**9 <= a <= 10**9 and -10**9 <= b <= 10**9:
-    f = open('output.txt', 'w')
+    f = open('../task1/output.txt', 'w')
     s = str(a+b)
     f.write(s)
     f.close()
--- AaDS lab0/task2/fibo1.py
+++ AaDS lab0/task2/fibo1.py
@@ -1,19 +1,19 @@
 import time 
 t_start = time.perf_counter()
 
-f = open('input.txt')
+f = open('../task2/input.txt')
 k = int(f.readline())
 f.close()
 if 0 < k <= 45:
     a, b = 0, 1
     for i in range(k-1):
         a, b = b, a+b
-    f = open('output.txt', 'w')
+    f = open('../task2/output.txt', 'w')
     f.write(str(b))
-    f.close() 
+    f.close()
     print("Время работы: %s секунд " % (time.perf_counter() - t_start))
 elif k == 0:
-    f = open('output.txt', 'w')
+    f = open('../task2/output.txt', 'w')
     f.write("0")
     f.close()
     print("Время работы: %s секунд " % (time.perf_counter() - t_start))
