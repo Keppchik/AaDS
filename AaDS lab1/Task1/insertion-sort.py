811ad7939f24519819578746a3fc335129e00f47
Keppchik <466353@niuitmo.ru>
2024-09-25 11:11:28 +0300

    Completed task 1 and 3

---
+++ AaDS lab1/Task1/insertion-sort.py
@@ -0,0 +1,24 @@
+import time
+import psutil
+t_start = time.perf_counter()
+
+f = open("input.txt", "r")
+n = int(f.readline())
+a = list(map(int, f.readline().split()))
+f.close()
+
+for i in range(1, n):
+    j = i
+    temp = a[i]
+    while j > 0 and a[j-1] > temp:
+        a[j], a[j-1] = a[j-1], a[j]
+        j -= 1
+    a[j] = temp
+
+f = open("output.txt", "w")
+for i in range(n):
+    f.write(str(a[i])+" ")
+f.close()
+
+print("Время работы: %s секунд " % (time.perf_counter() - t_start))
+print("Память:", psutil.Process().memory_info().rss / 1024 ** 2, "МБ")
